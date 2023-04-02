1195d7081b882fcd75490a616546736ec351ba7a
Harrison Parrish <107582376+Excellcius@users.noreply.github.com>
2023-04-01 23:06:40 -0600

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,9 +1,16 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 public class Product
 {
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
     public int InventoryLevel { get; set; }
+    public int ReorderPoint { get; set; }
+    public int LeadTime { get; set; }
 }
 
 public class ProductList : List<Product>
@@ -12,7 +19,6 @@ public class ProductList : List<Product>
     {
         Add(product);
     }
-
     public void RemoveProduct(Product product)
     {
         Remove(product);
@@ -20,30 +26,22 @@ public class ProductList : List<Product>
 
     public void UpdateProduct(Product product)
     {
-        // Find the product in the list and update its properties
+    // Find the product in the list and update its properties
         var index = IndexOf(product);
         if (index != -1)
         {
             this[index] = product;
         }
     }
-}
 
-public class PurchaseOrder
-{
-    public string Supplier { get; set; }
-    public DateTime DeliveryDate { get; set; }
-    public string OrderStatus { get; set; }
-}
-
-public class PurchaseOrderList : List<PurchaseOrder>
-{
     public void GeneratePurchaseOrder(Product product, int quantity)
+{
+    if (product.InventoryLevel + quantity < product.ReorderPoint)
     {
         var purchaseOrder = new PurchaseOrder
         {
             Supplier = "Supplier name",
-            DeliveryDate = DateTime.Today.AddDays(7),
+            DeliveryDate = DateTime.Today.AddDays(product.LeadTime),
             OrderStatus = "Pending"
         };
 
@@ -52,15 +50,26 @@ public class PurchaseOrderList : List<PurchaseOrder>
 
         Add(purchaseOrder);
     }
+}
+}
 
+public class PurchaseOrder
+    {
+    public string Supplier { get; set; }
+    public DateTime DeliveryDate { get; set; }
+    public string OrderStatus { get; set; }
+}
+
+public class PurchaseOrderList : List<PurchaseOrder>
+    {
     public void UpdatePurchaseOrder(PurchaseOrder purchaseOrder)
     {
-        // Find the purchase order in the list and update its properties
+// Find the purchase order in the list and update its properties
         var index = IndexOf(purchaseOrder);
         if (index != -1)
-        {
+            {
             this[index] = purchaseOrder;
-        }
+            }
     }
 }
 
@@ -74,27 +83,37 @@ public class SalesData
 }
 
 public class SalesDataList : List<SalesData>
-{
+    {
     public void GenerateReport(string reportType)
     {
-        // Generate report based on the specified report type
-        // ...
+// Generate report based on the specified report type
+// ...
     }
-
     public void AnalyzeSalesData()
     {
-        // Analyze sales data to identify trends and patterns
-        // ...
-    }
+    // Analyze sales data to identify trends and patterns
+    // ...
+}
+
+public decimal GetTotalRevenue()
+{
+    return this.Sum(sd => sd.Price * sd.Quantity);
+}
+
+public decimal GetTotalProfit()
+{
+    return this.Sum(sd => (sd.Price - ProductList.FirstOrDefault(p => p.Name == sd.ProductName).Cost) * sd.Quantity);
+}
 }
 
 public class PricingOptimizer
 {
-    public decimal SuggestOptimalPrice()
-    {
-        // Use market trends, competitor data, and customer behavior to suggest optimal price
-        // ...
-    }
+public decimal SuggestOptimalPrice()
+{
+// Use market trends, competitor data, and customer behavior to suggest optimal price
+// ...
+return 0;
+}
 }
 
 public class Customer
@@ -106,49 +125,163 @@ public class Customer
 }
 
 public class CustomerList : List<Customer>
-{
-    public void AddCustomer(Customer customer)
     {
+    public void AddCustomer(Customer customer)
+        {
         Add(customer);
     }
+public void RemoveCustomer(Customer customer)
+{
+    Remove(customer);
+}
 
-    public void RemoveCustomer(Customer customer)
+public void UpdateCustomer(Customer customer)
+{
+    // Find the customer in the list and update its properties
+    var index = IndexOf(customer);
+    if (index != -1)
     {
-        Remove(customer);
+        this[index] = customer;
     }
-
-    public void UpdateCustomer(Customer customer)
-    {
-        // Find the customer in the list and update its properties
-        var index = IndexOf(customer);
-        if (index != -1)
-        {
-            this[index] = customer;
-        }
-    }
+}
 }
 
 public class EcommercePlatform
+
+
+public void UpdatePlatform(EcommercePlatform platform)
 {
-    public string Name { get; set; }
-    public string ApiKey { get; set; }
-    public string AuthenticationCredentials { get; set; }
+// Find the platform in the list and update its properties
+var index = IndexOf(platform);
+if (index != -1)
+{
+this[index] = platform;
+}
+}
 }
 
-public class ForecastingEngine
+public class InventoryManagementSystem
 {
-    public void ForecastDemand()
+    private ProductList products;
+    private PurchaseOrderList purchaseOrders;
+    private SalesDataList salesData;
+    private PricingOptimizer pricingOptimizer;
+    private CustomerList customers;
+    private EcommercePlatformList ecommercePlatforms;
+    private ForecastingEngine forecastingEngine;
+    public InventoryManagementSystem()
     {
-        // Use machine learning algorithms to forecast future demand
-        // ...
-    }
+        products = new ProductList();
+        purchaseOrders = new PurchaseOrderList();
+        salesData = new SalesDataList();
+        pricingOptimizer = new PricingOptimizer();
+        customers = new CustomerList();
+        ecommercePlatforms = new EcommercePlatformList();
+        forecastingEngine = new ForecastingEngine();
+}
 
-    public void SuggestInventoryStrategy()
-    {
-        // Suggest inventory strategy based on the forecasted demand
-        // ...
-    }
+// Inventory Management Methods
+public void AddProduct(Product product)
+{
+    products.AddProduct(product);
+}
 
-    public void SuggestPricingStrategy()
-    {
-        // Suggest pricing
+public void RemoveProduct(Product product)
+{
+    products.RemoveProduct(product);
+}
+
+public void UpdateProduct(Product product)
+{
+    products.UpdateProduct(product);
+}
+
+public void SetReorderPoint(Product product, int reorderPoint)
+{
+    product.ReorderPoint = reorderPoint;
+}
+
+public void SetLeadTime(Product product, int leadTime)
+{
+    product.LeadTime = leadTime;
+}
+
+public void GeneratePurchaseOrder(Product product, int quantity)
+{
+    purchaseOrders.GeneratePurchaseOrder(product, quantity);
+}
+
+// Sales Analysis Methods
+public void AddSalesData(SalesData sales)
+{
+    salesData.Add(sales);
+}
+
+public void RemoveSalesData(SalesData sales)
+{
+    salesData.Remove(sales);
+}
+
+public void UpdateSalesData(SalesData sales)
+{
+    salesData.Update(sales);
+}
+
+public void GenerateSalesReport(string reportType)
+{
+    salesData.GenerateReport(reportType);
+}
+
+public void AnalyzeSalesData()
+{
+    salesData.AnalyzeSalesData();
+}
+
+// Pricing Optimization Methods
+public decimal SuggestOptimalPrice()
+{
+    return pricingOptimizer.SuggestOptimalPrice();
+}
+
+// Customer Management Methods
+public void AddCustomer(Customer customer)
+{
+    customers.AddCustomer(customer);
+}
+
+public void RemoveCustomer(Customer customer)
+{
+    customers.RemoveCustomer(customer);
+}
+
+public void UpdateCustomer(Customer customer)
+{
+    customers.UpdateCustomer(customer);
+}
+
+// Ecommerce Platform Management Methods
+public void AddEcommercePlatform(EcommercePlatform platform)
+{
+    ecommercePlatforms.AddPlatform(platform);
+}
+
+public void RemoveEcommercePlatform(EcommercePlatform platform)
+{
+    ecommercePlatforms.RemovePlatform(platform);
+}
+
+public void UpdateEcommercePlatform(EcommercePlatform platform)
+{
+    ecommercePlatforms.UpdatePlatform(platform);
+}
+
+// Forecasting Methods
+public void GenerateSalesForecast()
+{
+    forecastingEngine.GenerateSalesForecast();
+}
+
+public void GenerateInventoryForecast()
+{
+    forecastingEngine.GenerateInventoryForecast();
+}
